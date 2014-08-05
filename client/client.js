f19150e3d334b9aa72adf4199066e65632efe169
andrewliebchen <andrewliebchen@gmail.com>
2014-08-05 00:30:34 -0700

    Stub in panes

---
+++ client/client.js
@@ -0,0 +1,10 @@
+
+
+Template.mainNav.events({
+  // Stub in showing chats/ideas in pane-1
+  'click .nav-item' : function(event) {
+    $('body').toggleClass('show-pane-1');
+    $('.nav-item.active').removeClass('active');
+    $(event.target).addClass('active');
+  }
+});
